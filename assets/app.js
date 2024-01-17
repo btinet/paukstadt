bf1939a7348e2b64e2de929ab98d05896d446944
Benjamin Wagner <51082373+btinet@users.noreply.github.com>
2024-01-17 19:22:53 +0100

    add pin

--- assets/app.js
+++ assets/app.js
@@ -12,3 +12,21 @@ const $ = require('jquery');
 // the bootstrap module doesn't export/return anything
 require('bootstrap');
 
+(() => {
+    'use strict'
+
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    const forms = document.querySelectorAll('.needs-validation')
+
+    // Loop over them and prevent submission
+    Array.from(forms).forEach(form => {
+        form.addEventListener('submit', event => {
+            if (!form.checkValidity()) {
+                event.preventDefault()
+                event.stopPropagation()
+            }
+
+            form.classList.add('was-validated')
+        }, false)
+    })
+})()
